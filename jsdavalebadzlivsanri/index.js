f776d825ca62d39f516f38e2a9cd87bc1c4c6f55
Anri-Shainidze <anrianu2006@gmail.com>
2024-12-19 23:39:11 +0400

    Add files via upload

---
+++ jsdavalebadzlivsanri/index.js
@@ -0,0 +1,13 @@
+
+const txt = "hello world";
+
+const container = document.getElementById("container");
+
+
+txt.split("").forEach(ragac => {
+    const div = document.createElement("div")
+   
+    div.classList.add("char");
+    div.textContent = ragac;
+    container.appendChild(div);
+});
