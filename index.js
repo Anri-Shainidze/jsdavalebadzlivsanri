44e27c2f6cf82dfc3da295f16b35d7a2c36eb1ea
Anri-Shainidze <anrianu2006@gmail.com>
2024-12-19 23:34:42 +0400

    sad

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const container = document.getElementById("container");
 
 
 txt.split("").forEach(ragac => {
-    const div = document.createElement("div");
+    const div = document.createElement("div")
+   
     div.classList.add("char");
     div.textContent = ragac;
     container.appendChild(div);
